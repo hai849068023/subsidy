34ede8b4268135dd6176c32f1ce9a9119deba1eb
Liuxu <liuxu@example.com>
2021-02-27 16:50:58 +0800

    sql

--- subsidy.sql
+++ subsidy.sql
@@ -11,11 +11,56 @@
  Target Server Version : 50562
  File Encoding         : 65001
 
- Date: 26/02/2021 17:40:30
+ Date: 27/02/2021 16:50:43
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `p_img` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `describe` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `price` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `inventory` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for product_group
+-- ----------------------------
+DROP TABLE IF EXISTS `product_group`;
+CREATE TABLE `product_group`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `product_id` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for service
+-- ----------------------------
+DROP TABLE IF EXISTS `service`;
+CREATE TABLE `service`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `service_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `describe` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `introduce` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of service
+-- ----------------------------
+INSERT INTO `service` VALUES (1, '小保养', '轻松让您的发动机动力十足', NULL);
+INSERT INTO `service` VALUES (2, '大保养', '养护爱车，持久陪伴', NULL);
+INSERT INTO `service` VALUES (3, '深度养护', '深度养护爱车，爱车更具实力', NULL);
+INSERT INTO `service` VALUES (4, '汽车美容', '满足您对爱车的个性化追求', NULL);
 
 -- ----------------------------
 -- Table structure for users
